644663b1277c19a07db864a89c43874d83292f86
soridisaster <74194131+soridisaster@users.noreply.github.com>
2022-11-01 12:03:06 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//кнопка наверх
+window.addEventListener('scroll', function() {
+    
+    let currScrollPos2 = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (currScrollPos2 > 100) {
+      document.getElementById('toTop').style.opacity = currScrollPos2/500 ;
+    }
+    else  {
+        document.getElementById('toTop').style.opacity = -currScrollPos2/500 ;
+      }
+  }
+);
